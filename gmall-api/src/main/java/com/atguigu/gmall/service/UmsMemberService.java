60b2de6dbddd639b27a1e8fa670d7e874fb1eeea
DESKTOP-9BDA2NL\Lenovo <578854978@qq.com>
2020-03-01 21:05:00 +0800

    2020/3/1修改

---
+++ gmall-api/src/main/java/com/atguigu/gmall/service/UmsMemberService.java
@@ -0,0 +1,24 @@
+package com.atguigu.gmall.service;
+
+
+
+import com.atguigu.gmall.bean.UmsMember;
+
+import java.util.List;
+
+/**
+ * UmsMemberService
+ *
+ * @Author: wd
+ * @CreateTime: 2020-03-01
+ * @Description:
+ */
+public interface UmsMemberService {
+
+    List<UmsMember> getAllUser();
+
+
+    List<UmsMember> getAllUser2();
+
+    UmsMember getOne();
+}
